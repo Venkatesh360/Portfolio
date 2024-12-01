37db7a05e202bcaa95ea728356b7b0c3b67a2d6c
Venkatesh Kashyap <89914566+Venkatesh360@users.noreply.github.com>
2024-12-01 14:58:28 +0530

    Fixed animation

--- components/Details.tsx
+++ components/Details.tsx
@@ -52,13 +52,13 @@ function Details() {
                 </a>
             </div>
             <div className="flex items-center justify-center w-full gap-8">
-                <button className="hover:bg-gray-100 border-2 text-gray-100 hover:text-gray-800 font-semibold py-1 px-2 md:py-2 md:px-4 rounded inline-flex items-center" onClick={downloadResume}>
+                <button className="hover:bg-gray-100 border-[1px] sm:border-2 text-gray-100 hover:text-gray-800 font-semibold py-1 px-2 md:py-2 md:px-4 rounded inline-flex items-center" onClick={downloadResume}>
                     <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                     </svg>
                     <span className="flex gap-2"> <span className="hidden sm:block">Download</span> Resume</span>
                 </button>
-                <button className="hover:bg-gray-100 border-2 text-gray-100 hover:text-gray-800 font-semibold py-1 px-2 md:py-2 md:px-4 rounded inline-flex items-center" onClick={copyToClipboard}>
+                <button className="hover:bg-gray-100 border-[1px] sm:border-2 text-gray-100 hover:text-gray-800 font-semibold py-1 px-2 md:py-2 md:px-4 rounded inline-flex items-center" onClick={copyToClipboard}>
                     <FaCopy />
                     <span className="flex gap-2 ml-2"> <span className="hidden sm:flex">Copy</span> Email</span>
                 </button>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import './Hero.css';
 
 function Hero() {
     return (
-        <div className="main-cont flex items-center justify-center  w-full ">
+        <div className="main-cont flex items-center justify-center  w-full">
             <div className="flex flex-col items-center justify-center font-Karla font-[800] text-white">
                 <div className="container flex flex-col items-center justify-center">
                     <div className="create text-[10vh] sm:text-[20vw] md:text-[150px] lg:text-[200px] leading-none">
@@ -18,7 +18,7 @@ function Hero() {
                         </div>
                     </div>
                 </div>
-                <div className="box flex justify-center w-fit max-w-[50vw] md:max-w-xl h-auto bg-gray-900/80 items-center p-2 rounded-lg">
+                <div className="box flex justify-center -mb-6 sm:mb-0  w-[375px] md:max-w-xl h-auto bg-gray-900/80 items-center p-2 rounded-lg">
                     <p className=" text-[0.9rem] sm:text-[1rem] text-center tracking-normal" style={{ fontWeight: 300 }} >
                         Hi <span className="wave">👋</span>, I am Venkatesh Kashyap. I am a fullstack software developer based in Guwahati, Assam
                     </p>
